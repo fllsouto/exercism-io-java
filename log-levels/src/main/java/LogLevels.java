9ba484c711de2d1cbe221a86d96a91ed372a22ad
Fellipe Souto <fllsouto@gmail.com>
2022-06-18 17:59:52 -0300

    exercise: log-levels iteration 2

--- log-levels/src/main/java/LogLevels.java
+++ log-levels/src/main/java/LogLevels.java
@@ -1,17 +1,19 @@
 public class LogLevels {
     
     public static String message(String logLine) {
-        String[] lineTokens = logLine.split(":");
-        return lineTokens[1].trim();
+        return logLine
+            .split(":")[1]
+            .trim();
     }
 
     public static String logLevel(String logLine) {
-        String[] lineTokens = logLine.split(":");
-        return lineTokens[0].substring(1, lineTokens[0].length() - 1).toLowerCase();
+        return logLine
+            .split("]:")[0]
+            .substring(1)
+            .toLowerCase();
     }
 
     public static String reformat(String logLine) {
-        String s = String.format("%s (%s)", message(logLine), logLevel(logLine));
-        return s;
+        return String.format("%s (%s)", message(logLine), logLevel(logLine));
     }
 }
