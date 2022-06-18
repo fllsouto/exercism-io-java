0a37254ee635e43ab5e74cdbe5b45e414c010cf2
Fellipe Souto <fllsouto@gmail.com>
2022-06-18 18:44:29 -0300

    exercise: log-levels iteration 3 with regex

--- log-levels/src/main/java/LogLevels.java
+++ log-levels/src/main/java/LogLevels.java
@@ -1,19 +1,26 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LogLevels {
+
+    private static final String REGEX = "\\[(?<level>[A-Z]+)\\]\\:\\s+(?<message>.+)";
+    private static final Pattern PATTERN = Pattern.compile(REGEX);
     
     public static String message(String logLine) {
-        return logLine
-            .split(":")[1]
-            .trim();
+        return matches(logLine).group("message").trim();
     }
 
     public static String logLevel(String logLine) {
-        return logLine
-            .split("]:")[0]
-            .substring(1)
-            .toLowerCase();
+        return matches(logLine).group("level").toLowerCase();
     }
 
     public static String reformat(String logLine) {
         return String.format("%s (%s)", message(logLine), logLevel(logLine));
+    }
+
+    private static Matcher matches(String logLine) {
+        var matcher = PATTERN.matcher(logLine);
+        matcher.find();
+        return matcher;
     }
 }
