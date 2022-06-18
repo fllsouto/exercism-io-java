9c694c946f5ce2c372be135b6d2d0a93419f1934
Fellipe Souto <fllsouto@gmail.com>
2022-06-18 19:42:57 -0300

    exercise: reverse-string iteration 2

--- reverse-string/src/main/java/ReverseString.java
+++ reverse-string/src/main/java/ReverseString.java
@@ -1,11 +1,7 @@
 class ReverseString {
 
     String reverse(String inputString) {
-        String reversed = "";
-        for (int i = inputString.length() - 1; i >= 0; i--) {
-            reversed += inputString.charAt(i);
-        }
-        return reversed;
+        return new StringBuilder(inputString).reverse().toString();
     }
   
 }
