cc81ff1e9967f980c3c8db7ad9b75174988d497e
Fellipe Souto <fllsouto@gmail.com>
2022-06-22 11:26:21 -0300

    exercise: micro-blog iteration 1

---
+++ micro-blog/src/main/java/MicroBlog.java
@@ -0,0 +1,11 @@
+import java.util.stream.Collectors;
+
+class MicroBlog {
+    public String truncate(String input) {
+        return input
+            .codePoints()
+            .limit(5)
+            .mapToObj(c -> Character.toString(c))
+            .collect(Collectors.joining(""));
+    }
+}
