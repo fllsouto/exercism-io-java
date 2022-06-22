b2444dcdae12fda9bcff375a014f9b10c342f427
Fellipe Souto <fllsouto@gmail.com>
2022-06-22 11:29:17 -0300

    exercise: micro-blog iteration 2

--- micro-blog/src/main/java/MicroBlog.java
+++ micro-blog/src/main/java/MicroBlog.java
@@ -1,11 +1,14 @@
-import java.util.stream.Collectors;
-
 class MicroBlog {
+    private static final int MAX_CHARACTERS = 5;
+
     public String truncate(String input) {
         return input
             .codePoints()
-            .limit(5)
-            .mapToObj(c -> Character.toString(c))
-            .collect(Collectors.joining(""));
+            .limit(MAX_CHARACTERS)
+            .collect(
+                StringBuilder::new,
+                StringBuilder::appendCodePoint,
+                StringBuilder::append
+            ).toString();
     }
 }
